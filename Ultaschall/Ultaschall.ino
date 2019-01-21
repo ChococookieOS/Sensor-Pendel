592955d406ef086c041c3211bf8820e4a0185259
ChococookieOS <Chococookiemedia@gmail.com>
2019-01-21 12:08:23 +0100

    Ready Project

--- Ultaschall/Ultaschall.ino
+++ Ultaschall/Ultaschall.ino
@@ -1,15 +1,3 @@
-/*
-  HC-SR04 Ping distance sensor]
-  VCC to arduino 5v GND to arduino GND
-  Echo to Arduino pin 13 Trig to Arduino pin 12
-  Red POS to Arduino pin 11
-  Green POS to Arduino pin 10
-  560 ohm resistor to both LED NEG and GRD power rail
-  More info at: http://goo.gl/kJ8Gl
-  Original code improvements to the Ping sketch sourced from Trollmaker.com
-  Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
-*/
-
 #include <Servo.h>
 
 #define trigPin 12
@@ -23,28 +11,21 @@ Servo servo;
 
 void setup() {
   Serial.begin (9600);
+  Serial.println("Init");
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(powerBank, OUTPUT);
   servo.attach(servoPin);
-  servo.write(90);
-  delay(2000);
-  servo.write(45);
-  delay(250);
-  servo.write(135);
-  delay(250);
-  servo.write(90);
-  delay(2000);
+  delay(3000);
 }
 
 void loop() {
     digitalWrite(powerBank, HIGH);
     long duration, distance;
-    digitalWrite(trigPin, LOW);  // Added this line
-    delayMicroseconds(2); // Added this line
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
     digitalWrite(trigPin, HIGH);
-    //  delayMicroseconds(1000); - Removed this line
-    delayMicroseconds(10); // Added this line
+    delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
     duration = pulseIn(echoPin, HIGH);
     distance = (duration/2) / 29.1;
@@ -52,15 +33,12 @@ void loop() {
       Serial.println("Out of range");
     }
     else if(distance <= 10){
-      servo.write(0);
-      delay(180);
-      servo.write(90);
-      delay(4000);
       servo.write(180);
-      delay(180);
+      delay(4000);
+      servo.write(89);
     }
     Serial.print(distance);
     Serial.println(" cm");
-    delay(300);
-      digitalWrite(powerBank,LOW);
+    delay(100);
+    digitalWrite(powerBank,LOW);
 }
