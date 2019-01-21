cfff88918bee3301f483ad6fa6ed8e1e4be3f59e
ChococookieOS <Chococookiemedia@gmail.com>
2019-01-21 12:10:07 +0100

    Ready Project

---
+++ Sensor-Pendel/Sensor-Pendel.ino
@@ -0,0 +1,44 @@
+#include <Servo.h>
+
+#define trigPin 12
+#define echoPin 11
+
+#define servoPin 18
+
+#define powerBank 2
+
+Servo servo;
+
+void setup() {
+  Serial.begin (9600);
+  Serial.println("Init");
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(powerBank, OUTPUT);
+  servo.attach(servoPin);
+  delay(3000);
+}
+
+void loop() {
+    digitalWrite(powerBank, HIGH);
+    long duration, distance;
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = (duration/2) / 29.1;
+    if (distance >= 200 || distance <= 0){
+      Serial.println("Out of range");
+    }
+    else if(distance <= 10){
+      servo.write(180);
+      delay(4000);
+      servo.write(89);
+    }
+    Serial.print(distance);
+    Serial.println(" cm");
+    delay(100);
+    digitalWrite(powerBank,LOW);
+}
