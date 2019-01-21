9cee074c86aba137d18114c0996992665bdae55a
ChococookieOS <Chococookiemedia@gmail.com>
2019-01-21 12:35:53 +0100

    Reduced delay

--- Sensor-Pendel/Sensor-Pendel.ino
+++ Sensor-Pendel/Sensor-Pendel.ino
@@ -29,16 +29,11 @@ void loop() {
     digitalWrite(trigPin, LOW);
     duration = pulseIn(echoPin, HIGH);
     distance = (duration/2) / 29.1;
-    if (distance >= 200 || distance <= 0){
-      Serial.println("Out of range");
-    }
-    else if(distance <= 10){
+    if(distance <= 10){
       servo.write(180);
       delay(4000);
       servo.write(89);
     }
-    Serial.print(distance);
-    Serial.println(" cm");
-    delay(100);
+    delay(20);
     digitalWrite(powerBank,LOW);
 }
